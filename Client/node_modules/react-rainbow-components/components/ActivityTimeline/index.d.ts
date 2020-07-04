1a61e08830b1deba93e1f44e0b1ca5cf6b2de333
nirbhay.bagmar <nirbhay.bagmar@hashedin.com>
2020-07-04 11:38:39 +0530

    Removed unwanted files

--- Client/node_modules/react-rainbow-components/components/ActivityTimeline/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-import { BaseProps } from '../types';
-
-export interface ActivityTimelineProps extends BaseProps {
-    children?: ReactNode;
-}
-
-export default function(props: ActivityTimelineProps): JSX.Element | null;
--- Client/node_modules/react-rainbow-components/components/ButtonGroup/index.d.ts
+++
@@ -1,8 +0,0 @@
-import { ReactNode } from 'react';
-import { BaseProps } from '../types';
-
-export interface ButtonGroupProps extends BaseProps {
-    children?: ReactNode;
-}
-
-export default function(props: ButtonGroupProps): JSX.Element | null;
